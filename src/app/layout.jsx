5d7c8d69d15e9e534e00f0c3bb38d8d9589dcc24
kidui23 <215383363+kidui23@users.noreply.github.com>
2026-05-29 01:53:01 +0300

    Updated responsive layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import "../styles/globals.css";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/layout/sidebar";
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
 import MobileNav from "../components/layout/mobile-nav";
